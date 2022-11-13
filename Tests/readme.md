6721656e130cd643a3f4a6fdda0cdef859997b56
Joona Nousiainen <joona.nousiainen@tuni.fi>
2022-11-13 19:14:36 +0200

    refactor project structure

--- Tests/readme.md
+++
@@ -1,5 +0,0 @@
-# Unit tests for assignment part 2
-
-## How to run tests
-npm install --save mocha chai
-npm run test
--- Tests/tests.js
+++ Tests/tests.js
@@ -4,16 +4,16 @@ Unit tests for assignment part 2
 Joona Nousiainen
 Seyda Baysal
 */
-import ceil from '../COMP.SE.200-2022-2023-1-main/src/ceil.js';
-import words from '../COMP.SE.200-2022-2023-1-main/src/words.js';
-import islength from '../COMP.SE.200-2022-2023-1-main/src/isLength.js';
-import upperfirst from '../COMP.SE.200-2022-2023-1-main/src/upperFirst';
-import add from '../COMP.SE.200-2022-2023-1-main/src/add.js';
-import divide from '../COMP.SE.200-2022-2023-1-main/src/divide';
-import isempty from '../COMP.SE.200-2022-2023-1-main/src/isEmpty';
-import get from '../COMP.SE.200-2022-2023-1-main/src/get';
-import defaulttoany from '../COMP.SE.200-2022-2023-1-main/src/defaultToAny.js';
-import eq from '../COMP.SE.200-2022-2023-1-main/src/eq.js';
+import ceil from '../lib/src/ceil.js';
+import words from '../lib/src/words.js';
+import islength from '../lib/src/isLength.js';
+import upperfirst from '../lib/src/upperFirst.js';
+import add from '../lib/src/add.js';
+import divide from '../lib/src/divide.js';
+import isempty from '../lib/src/isEmpty.js';
+import get from '../lib/src/get.js';
+import defaulttoany from '../lib/src/defaultToAny.js';
+import eq from '../lib/src/eq.js';
 /* Chai library for all the required asserting functions*/
 import { expect } from 'chai';
 
